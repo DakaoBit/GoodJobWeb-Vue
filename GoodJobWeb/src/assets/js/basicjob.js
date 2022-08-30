591abb5859b03f993e6dc6b8d150ac5b22134f71
StanWang <oasis.wang1016@gmail.com>
2022-08-30 15:12:54 +0800

    [新增] 新增BasicJob頁面

---
+++ GoodJobWeb/src/assets/js/basicjob.js
@@ -0,0 +1,32 @@
+import { createApp } from 'vue'
+import '../lib/tom-select/tom-select.css'
+import '../css/style.css'
+import { fixedHeader } from '../../components/StickyHeader.vue'
+import StickyHeader from '../../components/StickyHeader.vue'
+import BasicJob from '../../views/BasicJob.vue'
+
+document.addEventListener("DOMContentLoaded", function () {
+  // When the user scrolls the page, execute fixedHeader
+  window.onscroll = function () {
+    fixedHeader();
+  };
+
+  new TomSelect("#type", {
+    create: false,
+    sortField: {
+      field: "text",
+      direction: "asc",
+    },
+  });
+
+  new TomSelect("#level", {
+    create: false,
+    sortField: {
+      field: "text",
+      direction: "asc",
+    },
+  });
+});
+
+createApp(StickyHeader).mount('#sticky-header')
+createApp(BasicJob).mount('#basicjob')
