9d0d1ec0e8a5154e2943c09436446c02af2b9b03
oasis.wang <oasis.wang1016@gmail.com>
2022-09-24 13:57:16 +0800

    [調整] 1. 增加Cookie.js, 2.首頁要進PostJob頁面時必須有cookie: userToken

--- GoodJobWeb/src/components/StickyHeader.vue
+++ GoodJobWeb/src/components/StickyHeader.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="row">
     <div class="j_border col p-3 bg_dark btn_dark">
-      <a href="/SignIn.html" class="h2">
-        Company Sign In
-      </a>
+      <a href="/SignIn.html" class="h2"> Company Sign In </a>
     </div>
   </div>
   <div class="row">
     <div class="j_border col p-3 bg_basic btn_basic">
-      <a href="/Index.html" class="h2">
-        Find Job
-      </a>
+      <a href="/Index.html" class="h2"> Find Job </a>
     </div>
     <div class="j_border col p-3 bg_premium btn_premium">
-      <a href="/PostJob.html" class="h2">
-        Post Job
-      </a>
+      <a @click="PostJob" class="h2"> Post Job </a>
     </div>
   </div>
-
 </template>
 
+<script setup>
+// var obj = {
+//   name: "userToken",
+//   value: "GJGTTAAGG",
+//   day: 1,
+// };
+// setCookie(obj);
+
+const PostJob = async () => {
+  //必須拿到userToken
+  if (_.isEmpty(getCookie("userToken"))) {
+    swalTip("Warning", "You must be sign in!");
+  } else {
+    location.href = "/PostJob.html";
+  }
+};
+</script>
 <script>
 /**
  * 固定Header
